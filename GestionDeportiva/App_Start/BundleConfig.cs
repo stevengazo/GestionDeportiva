e9fe714d3022c9c64a64a5b36b4e442ba4b3027d
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2023-03-26 21:09:56 -0600

    Actualización Bootstrapp y Jquery

--- GestionDeportiva/App_Start/BundleConfig.cs
+++ GestionDeportiva/App_Start/BundleConfig.cs
@@ -19,8 +19,9 @@ namespace GestionDeportiva
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
 						"~/Scripts/modernizr-*"));
 
-			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-					  "~/Scripts/bootstrap.js"));
+			bundles.Add(new Bundle("~/bundles/bootstrap").Include(
+								  "~/Scripts/bootstrap.js",
+								  "~/Scripts/respond.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 					  "~/Content/bootstrap.css",
